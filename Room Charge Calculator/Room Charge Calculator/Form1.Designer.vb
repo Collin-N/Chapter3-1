61555e863a095d53cd9da8ca4348b13a0048dcc3
Collin N <bubba9n500@gmail.com>
2017-11-15 09:26:01 -0600

    update2

--- Room Charge Calculator/Room Charge Calculator/Form1.Designer.vb
+++ Room Charge Calculator/Room Charge Calculator/Form1.Designer.vb
@@ -25,31 +25,34 @@ Partial Class Form1
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lblDateToday = New System.Windows.Forms.Label()
+        Me.lblTimeToday = New System.Windows.Forms.Label()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TxtNightCharges = New System.Windows.Forms.TextBox()
+        Me.txtNights = New System.Windows.Forms.TextBox()
         Me.Label7 = New System.Windows.Forms.Label()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
         Me.Label10 = New System.Windows.Forms.Label()
         Me.Label9 = New System.Windows.Forms.Label()
         Me.Label8 = New System.Windows.Forms.Label()
-        Me.TextBox5 = New System.Windows.Forms.TextBox()
-        Me.TextBox4 = New System.Windows.Forms.TextBox()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.txtMisc = New System.Windows.Forms.TextBox()
+        Me.TxtTelephone = New System.Windows.Forms.TextBox()
+        Me.TxtRoomService = New System.Windows.Forms.TextBox()
         Me.GroupBox3 = New System.Windows.Forms.GroupBox()
-        Me.Label20 = New System.Windows.Forms.Label()
-        Me.Label19 = New System.Windows.Forms.Label()
-        Me.Label18 = New System.Windows.Forms.Label()
-        Me.Label17 = New System.Windows.Forms.Label()
-        Me.Label16 = New System.Windows.Forms.Label()
+        Me.lblTotalCharges = New System.Windows.Forms.Label()
+        Me.lblTax = New System.Windows.Forms.Label()
+        Me.lblSubtotal = New System.Windows.Forms.Label()
+        Me.lblAdditionalCharges = New System.Windows.Forms.Label()
+        Me.lblRoomCharges = New System.Windows.Forms.Label()
         Me.Label15 = New System.Windows.Forms.Label()
         Me.Label14 = New System.Windows.Forms.Label()
         Me.Label13 = New System.Windows.Forms.Label()
         Me.Label12 = New System.Windows.Forms.Label()
         Me.Label11 = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
         Me.GroupBox1.SuspendLayout()
         Me.GroupBox2.SuspendLayout()
         Me.GroupBox3.SuspendLayout()
@@ -83,24 +86,24 @@ Partial Class Form1
         Me.Label3.TabIndex = 2
         Me.Label3.Text = "Time:"
         '
-        'Label4
+        'lblDateToday
         '
-        Me.Label4.Location = New System.Drawing.Point(152, 58)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(167, 23)
-        Me.Label4.TabIndex = 3
+        Me.lblDateToday.Location = New System.Drawing.Point(152, 58)
+        Me.lblDateToday.Name = "lblDateToday"
+        Me.lblDateToday.Size = New System.Drawing.Size(167, 23)
+        Me.lblDateToday.TabIndex = 3
         '
-        'Label5
+        'lblTimeToday
         '
-        Me.Label5.Location = New System.Drawing.Point(155, 82)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(164, 25)
-        Me.Label5.TabIndex = 4
+        Me.lblTimeToday.Location = New System.Drawing.Point(155, 82)
+        Me.lblTimeToday.Name = "lblTimeToday"
+        Me.lblTimeToday.Size = New System.Drawing.Size(164, 25)
+        Me.lblTimeToday.TabIndex = 4
         '
         'GroupBox1
         '
-        Me.GroupBox1.Controls.Add(Me.TextBox2)
-        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.TxtNightCharges)
+        Me.GroupBox1.Controls.Add(Me.txtNights)
         Me.GroupBox1.Controls.Add(Me.Label7)
         Me.GroupBox1.Controls.Add(Me.Label6)
         Me.GroupBox1.Location = New System.Drawing.Point(12, 141)
@@ -110,19 +113,19 @@ Partial Class Form1
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Room Information"
         '
-        'TextBox2
+        'TxtNightCharges
         '
-        Me.TextBox2.Location = New System.Drawing.Point(81, 49)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox2.TabIndex = 3
+        Me.TxtNightCharges.Location = New System.Drawing.Point(81, 49)
+        Me.TxtNightCharges.Name = "TxtNightCharges"
+        Me.TxtNightCharges.Size = New System.Drawing.Size(100, 20)
+        Me.TxtNightCharges.TabIndex = 3
         '
-        'TextBox1
+        'txtNights
         '
-        Me.TextBox1.Location = New System.Drawing.Point(81, 16)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox1.TabIndex = 2
+        Me.txtNights.Location = New System.Drawing.Point(81, 16)
+        Me.txtNights.Name = "txtNights"
+        Me.txtNights.Size = New System.Drawing.Size(100, 20)
+        Me.txtNights.TabIndex = 2
         '
         'Label7
         '
@@ -147,9 +150,9 @@ Partial Class Form1
         Me.GroupBox2.Controls.Add(Me.Label10)
         Me.GroupBox2.Controls.Add(Me.Label9)
         Me.GroupBox2.Controls.Add(Me.Label8)
-        Me.GroupBox2.Controls.Add(Me.TextBox5)
-        Me.GroupBox2.Controls.Add(Me.TextBox4)
-        Me.GroupBox2.Controls.Add(Me.TextBox3)
+        Me.GroupBox2.Controls.Add(Me.txtMisc)
+        Me.GroupBox2.Controls.Add(Me.TxtTelephone)
+        Me.GroupBox2.Controls.Add(Me.TxtRoomService)
         Me.GroupBox2.Location = New System.Drawing.Point(233, 141)
         Me.GroupBox2.Name = "GroupBox2"
         Me.GroupBox2.Size = New System.Drawing.Size(200, 125)
@@ -184,34 +187,34 @@ Partial Class Form1
         Me.Label8.TabIndex = 3
         Me.Label8.Text = "Room Service:"
         '
-        'TextBox5
+        'txtMisc
         '
-        Me.TextBox5.Location = New System.Drawing.Point(94, 86)
-        Me.TextBox5.Name = "TextBox5"
-        Me.TextBox5.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox5.TabIndex = 2
+        Me.txtMisc.Location = New System.Drawing.Point(94, 86)
+        Me.txtMisc.Name = "txtMisc"
+        Me.txtMisc.Size = New System.Drawing.Size(100, 20)
+        Me.txtMisc.TabIndex = 2
         '
-        'TextBox4
+        'TxtTelephone
         '
-        Me.TextBox4.Location = New System.Drawing.Point(94, 52)
-        Me.TextBox4.Name = "TextBox4"
-        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox4.TabIndex = 1
+        Me.TxtTelephone.Location = New System.Drawing.Point(94, 52)
+        Me.TxtTelephone.Name = "TxtTelephone"
+        Me.TxtTelephone.Size = New System.Drawing.Size(100, 20)
+        Me.TxtTelephone.TabIndex = 1
         '
-        'TextBox3
+        'TxtRoomService
         '
-        Me.TextBox3.Location = New System.Drawing.Point(94, 16)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox3.TabIndex = 0
+        Me.TxtRoomService.Location = New System.Drawing.Point(94, 16)
+        Me.TxtRoomService.Name = "TxtRoomService"
+        Me.TxtRoomService.Size = New System.Drawing.Size(100, 20)
+        Me.TxtRoomService.TabIndex = 0
         '
         'GroupBox3
         '
-        Me.GroupBox3.Controls.Add(Me.Label20)
-        Me.GroupBox3.Controls.Add(Me.Label19)
-        Me.GroupBox3.Controls.Add(Me.Label18)
-        Me.GroupBox3.Controls.Add(Me.Label17)
-        Me.GroupBox3.Controls.Add(Me.Label16)
+        Me.GroupBox3.Controls.Add(Me.lblTotalCharges)
+        Me.GroupBox3.Controls.Add(Me.lblTax)
+        Me.GroupBox3.Controls.Add(Me.lblSubtotal)
+        Me.GroupBox3.Controls.Add(Me.lblAdditionalCharges)
+        Me.GroupBox3.Controls.Add(Me.lblRoomCharges)
         Me.GroupBox3.Controls.Add(Me.Label15)
         Me.GroupBox3.Controls.Add(Me.Label14)
         Me.GroupBox3.Controls.Add(Me.Label13)
@@ -224,45 +227,45 @@ Partial Class Form1
         Me.GroupBox3.TabStop = False
         Me.GroupBox3.Text = "Total Charges"
         '
-        'Label20
+        'lblTotalCharges
         '
-        Me.Label20.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.Label20.Location = New System.Drawing.Point(233, 149)
-        Me.Label20.Name = "Label20"
-        Me.Label20.Size = New System.Drawing.Size(86, 21)
-        Me.Label20.TabIndex = 9
+        Me.lblTotalCharges.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTotalCharges.Location = New System.Drawing.Point(233, 149)
+        Me.lblTotalCharges.Name = "lblTotalCharges"
+        Me.lblTotalCharges.Size = New System.Drawing.Size(86, 21)
+        Me.lblTotalCharges.TabIndex = 9
         '
-        'Label19
+        'lblTax
         '
-        Me.Label19.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.Label19.Location = New System.Drawing.Point(233, 110)
-        Me.Label19.Name = "Label19"
-        Me.Label19.Size = New System.Drawing.Size(86, 24)
-        Me.Label19.TabIndex = 8
+        Me.lblTax.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTax.Location = New System.Drawing.Point(233, 110)
+        Me.lblTax.Name = "lblTax"
+        Me.lblTax.Size = New System.Drawing.Size(86, 24)
+        Me.lblTax.TabIndex = 8
         '
-        'Label18
+        'lblSubtotal
         '
-        Me.Label18.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.Label18.Location = New System.Drawing.Point(233, 73)
-        Me.Label18.Name = "Label18"
-        Me.Label18.Size = New System.Drawing.Size(86, 25)
-        Me.Label18.TabIndex = 7
+        Me.lblSubtotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblSubtotal.Location = New System.Drawing.Point(233, 73)
+        Me.lblSubtotal.Name = "lblSubtotal"
+        Me.lblSubtotal.Size = New System.Drawing.Size(86, 25)
+        Me.lblSubtotal.TabIndex = 7
         '
-        'Label17
+        'lblAdditionalCharges
         '
-        Me.Label17.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.Label17.Location = New System.Drawing.Point(233, 42)
-        Me.Label17.Name = "Label17"
-        Me.Label17.Size = New System.Drawing.Size(86, 24)
-        Me.Label17.TabIndex = 6
+        Me.lblAdditionalCharges.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblAdditionalCharges.Location = New System.Drawing.Point(233, 42)
+        Me.lblAdditionalCharges.Name = "lblAdditionalCharges"
+        Me.lblAdditionalCharges.Size = New System.Drawing.Size(86, 24)
+        Me.lblAdditionalCharges.TabIndex = 6
         '
-        'Label16
+        'lblRoomCharges
         '
-        Me.Label16.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.Label16.Location = New System.Drawing.Point(233, 16)
-        Me.Label16.Name = "Label16"
-        Me.Label16.Size = New System.Drawing.Size(86, 21)
-        Me.Label16.TabIndex = 5
+        Me.lblRoomCharges.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblRoomCharges.Location = New System.Drawing.Point(233, 16)
+        Me.lblRoomCharges.Name = "lblRoomCharges"
+        Me.lblRoomCharges.Size = New System.Drawing.Size(86, 21)
+        Me.lblRoomCharges.TabIndex = 5
         '
         'Label15
         '
@@ -309,16 +312,46 @@ Partial Class Form1
         Me.Label11.TabIndex = 0
         Me.Label11.Text = "Tax:"
         '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(48, 461)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(104, 23)
+        Me.btnCalculate.TabIndex = 6
+        Me.btnCalculate.Text = "Calculate Charges"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(187, 461)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 7
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(294, 461)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 8
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(445, 487)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox3)
         Me.Controls.Add(Me.GroupBox1)
-        Me.Controls.Add(Me.Label5)
-        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.lblTimeToday)
+        Me.Controls.Add(Me.lblDateToday)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
@@ -338,30 +371,33 @@ Partial Class Form1
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents Label3 As System.Windows.Forms.Label
-    Friend WithEvents Label4 As System.Windows.Forms.Label
-    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents lblDateToday As System.Windows.Forms.Label
+    Friend WithEvents lblTimeToday As System.Windows.Forms.Label
     Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
-    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
-    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TxtNightCharges As System.Windows.Forms.TextBox
+    Friend WithEvents txtNights As System.Windows.Forms.TextBox
     Friend WithEvents Label7 As System.Windows.Forms.Label
     Friend WithEvents Label6 As System.Windows.Forms.Label
     Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
     Friend WithEvents Label10 As System.Windows.Forms.Label
     Friend WithEvents Label9 As System.Windows.Forms.Label
     Friend WithEvents Label8 As System.Windows.Forms.Label
-    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
-    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
-    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents txtMisc As System.Windows.Forms.TextBox
+    Friend WithEvents TxtTelephone As System.Windows.Forms.TextBox
+    Friend WithEvents TxtRoomService As System.Windows.Forms.TextBox
     Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
-    Friend WithEvents Label20 As System.Windows.Forms.Label
-    Friend WithEvents Label19 As System.Windows.Forms.Label
-    Friend WithEvents Label18 As System.Windows.Forms.Label
-    Friend WithEvents Label17 As System.Windows.Forms.Label
-    Friend WithEvents Label16 As System.Windows.Forms.Label
+    Friend WithEvents lblTotalCharges As System.Windows.Forms.Label
+    Friend WithEvents lblTax As System.Windows.Forms.Label
+    Friend WithEvents lblSubtotal As System.Windows.Forms.Label
+    Friend WithEvents lblAdditionalCharges As System.Windows.Forms.Label
+    Friend WithEvents lblRoomCharges As System.Windows.Forms.Label
     Friend WithEvents Label15 As System.Windows.Forms.Label
     Friend WithEvents Label14 As System.Windows.Forms.Label
     Friend WithEvents Label13 As System.Windows.Forms.Label
     Friend WithEvents Label12 As System.Windows.Forms.Label
     Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
 
 End Class
--- Room Charge Calculator/Room Charge Calculator/Form1.vb
+++ Room Charge Calculator/Room Charge Calculator/Form1.vb
@@ -1,4 +1,38 @@
 ﻿Public Class Form1
 
   
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim decRoomCharges As Decimal
+        Dim decAddCharges As Decimal
+        Dim decSubtotal As Decimal
+        Dim decTax As Decimal
+        Dim decTotal As Decimal
+        Const decTAX_RATE As Decimal = 0.08D
+
+        lblStatus.Text = String.Empty
+        Try
+            decRoomCharges = CDec(txtNights.Text) * CDec(TxtNightCharges.Text)
+            lblRoomCharges.Text = decRoomCharges.ToString("c")
+            decAddCharges = CDec(TxtRoomService.Text) + CDec(TxtTelephone.Text) + CDec(txtMisc.Text)
+            lblAdditionalCharges.Text = decAddCharges.ToString("c")
+
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblDateToday.Text = Now.ToString("D")
+        lblTimeToday.Text = Now.ToString("T")
+
+    End Sub
 End Class
